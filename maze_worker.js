904d7ff2005f289ab8f6130a663840f4c282a8ab
Pavan Kuppa <936542+pavankuppa@users.noreply.github.com>
2025-10-16 11:10:02 +0530

    Update maze_worker.js

--- maze_worker.js
+++ maze_worker.js
@@ -4,7 +4,7 @@ self.onmessage = function(request) {
     const wallProbability = request.data.wallProbability
     const boxSize = request.data.boxSize
 
-    const walls = []
+    let walls = []
 
     for(let i = boxSize+1; i <= height; i+=boxSize+1){
         for(let j = boxSize+1; j <= width; j+=boxSize+1){
@@ -12,6 +12,8 @@ self.onmessage = function(request) {
             walls.push({ x: j-(boxSize+1), y: i, type: "v" })
         }
     }
+
+    walls = shuffleWalls(walls)
 
     let counts = 0;
     const total = Math.ceil(walls.length*wallProbability/100)
@@ -21,5 +23,12 @@ self.onmessage = function(request) {
         walls.splice(index, 1)
         counts++;
     }
+}
 
+function shuffleWalls(walls) {
+    for (let i = walls.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // random index
+        [walls[i], walls[j]] = [walls[j], walls[i]];   // swap
+    }
+    return walls;
 }
